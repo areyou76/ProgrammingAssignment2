e91618438697ef1cb5b5dca86b1ce56626e2b418
areyou76 <yanty@salam.uitm.edu.my>
2015-06-21 15:13:00 +0800

    Third commit

--- cachematrix.R
+++ cachematrix.R
@@ -44,25 +44,18 @@ cacheSolve <- function(x, ...) {
   
   
   inv <- x$getinverse()
-  
   if(!is.null(inv)) {
-    
     message("getting cached data")
-    
     return(inv)
     
   }
+  
   data <- x$get()
-  
-  
   inv <- solve(data)
-  
   x$setinverse(inv)
   
-  
   inv
-  
-  
+ 
 }
 
 
